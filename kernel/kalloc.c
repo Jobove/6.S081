3156a73ae21aaa1bd7c68d46cddcda3349f04b9c
Kasugano Sora <Kasugano_Sora@m.scnu.edu.cn>
2023-05-05 17:57:15 +0800

    Finish the lab and got full score.

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -14,6 +14,8 @@ void freerange(void *pa_start, void *pa_end);
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
 
+extern char ref_count[];
+
 struct run {
   struct run *next;
 };
@@ -23,10 +25,16 @@ struct {
   struct run *freelist;
 } kmem;
 
+extern struct {
+  struct spinlock lock;
+  char *ref_count;
+} ref;
+
 void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
+  initlock(&ref.lock, "ref");
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -35,8 +43,12 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE) {
+    acquire(&ref.lock);
+    ref_count[PAGE_INDEX((uint64)p)] = 1;
+    release(&ref.lock);
     kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -50,6 +62,17 @@ kfree(void *pa)
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
+  
+  int idx = PAGE_INDEX((uint64)pa);
+
+  if (ref_count[idx] <= 0)
+    panic("kfree: ref_count less than zero");
+  acquire(&ref.lock);
+  if (--ref_count[idx] > 0) {
+    release(&ref.lock);
+    return;
+  }
+  release(&ref.lock);
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
@@ -76,7 +99,14 @@ kalloc(void)
     kmem.freelist = r->next;
   release(&kmem.lock);
 
-  if(r)
+  if(r) {
+    int idx = PAGE_INDEX((uint64)r);
     memset((char*)r, 5, PGSIZE); // fill with junk
+    acquire(&ref.lock);
+    if (ref_count[idx] != 0)
+      panic("kalloc: new page ref count not 0");
+    ref_count[idx] = 1;
+    release(&ref.lock);
+  }
   return (void*)r;
 }
