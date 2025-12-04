2ec258598de303d28b164c4348295ed3bfa18f8d
Jobove <Jobove@foxmail.com>
2025-12-05 00:03:59 +0800

    Turns out the last commit only passes when CPUS=1. This time the code is correct.

--- kernel/bio.c
+++ kernel/bio.c
@@ -29,6 +29,8 @@
 struct buf bufs[NBUF];
 
 struct bucket {
+  // struct spinlock append_lock;
+  // struct spinlock evict_lock;
   struct spinlock per_bucket_lock;
 
   // Linked list of all buffers, through prev/next.
@@ -50,14 +52,15 @@ binit(void)
 {
   struct buf *b;
   struct bucket *bucket;
+  char *lock_name;
 
   initlock(&buckets.buckets_lock, "bcache.buckets_lock");
 
   for (int i = 0; i < NBUCKET; ++i) {
     bucket = buckets.bucket + i;
-    char *lock_name = "bcache.per_bucket_lock_00";
-    lock_name[23] += i / 10;
-    lock_name[24] += i % 10;
+    lock_name = "bcache.per_bucket_lock_00";
+    lock_name[sizeof(lock_name) - 3] += i / 10;
+    lock_name[sizeof(lock_name) - 2] += i % 10;
 
     initlock(&bucket->per_bucket_lock, lock_name);
 
@@ -77,6 +80,25 @@ binit(void)
   }
 }
 
+// Check if a box of blockno exists.
+struct buf *
+check(uint dev, uint blockno) {
+  int index = INDEX(blockno);
+  struct buf *b = 0;
+  struct bucket *bucket = &buckets.bucket[index];
+
+  for (b = bucket->head.next; b != &bucket->head; b = b->next) {
+    if (b->dev != dev || b->blockno != blockno)
+      continue;
+
+    break;
+  }
+
+  if (b == &bucket->head)
+    return 0;
+  return b;
+}
+
 // Look through buffer cache for block on device dev.
 // If not found, allocate a buffer.
 // In either case, return locked buffer.
@@ -84,56 +106,65 @@ static struct buf*
 bget(uint dev, uint blockno)
 {
   int index = INDEX(blockno);
-  struct buf *b;
+  struct buf *existed;
   struct bucket *bucket = &buckets.bucket[index];
   struct spinlock *lock = &bucket->per_bucket_lock;
 
   acquire(lock);
 
   // Is the block already cached?
-  for(b = bucket->head.next; b != &bucket->head; b = b->next){
-    if(b->dev == dev && b->blockno == blockno){
-      b->refcnt++;
-      release(lock);
-      acquiresleep(&b->lock);
-      return b;
-    }
+  existed = check(dev, blockno);
+  if (existed) {
+    existed->refcnt++;
+    existed->timestamp = ticks;
+    release(lock);
+    acquiresleep(&existed->lock);
+    return existed;
   }
 
+  release(lock);
   // Not cached.
   /*
-    Acquire buckets_lock first before trying to perform substitution
-    should avoid deadlock when two threads holding other's required lock
-    acquire other's lock because the lock prevents more than one thread
-    from trying to substitute.
+    buckets.lock prevent other threads from substituting an existing block.
+    so even if we do not hold the `lock`, there won't exist any copy of blockno.
   */
   acquire(&buckets.buckets_lock);
 
+  acquire(lock);
+  existed = check(dev, blockno);
+  if (existed) {
+    existed->refcnt++;
+    existed->timestamp = ticks;
+    release(lock);
+    release(&buckets.buckets_lock);
+    acquiresleep(&existed->lock);
+    return existed;
+  }
+  release(lock);
+
   uint64 lru = __UINT32_MAX__;
   struct buf *lru_buf = 0;
-  // hold the source bucket lock until we find a lru-er one
+  // hold the current lru bucket lock until we find a lru-er one
   struct bucket *lru_bucket = 0;
 
   for (int i = 0; i < NBUCKET; ++i) {
     struct bucket *this_bucket = buckets.bucket + i;
-    
-    if (this_bucket != bucket)
-      acquire(&this_bucket->per_bucket_lock);
-    for (b = this_bucket->head.next; b != &this_bucket->head; b = b->next) {
+    acquire(&this_bucket->per_bucket_lock);
+
+    for (struct buf *b = this_bucket->head.next; b != &this_bucket->head; b = b->next) {
       if (b->refcnt != 0 || b->timestamp >= lru) {
         continue;
       }
 
+      lru_buf = b;
+      lru = b->timestamp;
       // if the last one belongs to a different bucket
       // release its lock
       if (lru_bucket != this_bucket) {
         if (lru_bucket)
           release(&lru_bucket->per_bucket_lock);
         lru_bucket = this_bucket;
-        lru_buf = b;
       }
-
-      lru = b->timestamp;
     }
 
     // if we cannot find a better candidate, release the lock of current bucket
@@ -149,7 +180,7 @@ bget(uint dev, uint blockno)
 
   // Extract lru_buf from its previous bucket
   if (lru_buf->next == &lru_bucket->head) {
-    // if it's the end of the list, we should change its prev->next to head 
+    // if it's the end of the list, we should change its prev->next to head
     // and change head->prev to the second last one
     lru_buf->prev->next = &lru_bucket->head;
     lru_bucket->head.prev = lru_buf->prev;
@@ -163,16 +194,19 @@ bget(uint dev, uint blockno)
   release(&lru_bucket->per_bucket_lock);
 
   // Insert lru_buf to the front of destination bucket
+  acquire(lock);
+
   lru_buf->prev = &bucket->head;
   lru_buf->next = bucket->head.next;
   bucket->head.next->prev = lru_buf;        // change previous front's prev from head to lru_buf
-  bucket->head.next = lru_buf;          // change head's next to lru_buf
+  bucket->head.next = lru_buf;              // change head's next to lru_buf
 
   lru_buf->dev = dev;
   lru_buf->blockno = blockno;
   lru_buf->valid = 0;
   lru_buf->refcnt = 1;
 
+  release(lock);
   release(&buckets.buckets_lock);
   acquiresleep(&lru_buf->lock);
 
@@ -243,5 +277,3 @@ bunpin(struct buf *b) {
   b->refcnt--;
   release(lock);
 }
-
-
